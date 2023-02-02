649220a6adb34f3d371318a135d020bb84d8d1b4
Siddhesh <sidpgdac@gmail.com>
2023-02-02 15:34:44 +0530

    third commit

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,6 +1,6 @@
 
 class BubbleSort {
-    static void insertionsort(int arr[]) {
+    static void selectionsort(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++)
             for (int j = 0; j < n - i - 1; j++)
@@ -15,5 +15,14 @@ class BubbleSort {
     public static void main(String args[]) {
 
         insertionsort();
+
+        switch (key) {
+            case value:
+
+                break;
+
+            default:
+                break;
+        }
     }
 }
