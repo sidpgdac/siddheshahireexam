4dd6ff831cea96dd123b306b6014cd29f0baf154
Siddhesh <sidpgdac@gmail.com>
2023-02-02 15:28:52 +0530

    first commit

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,23 +1,19 @@
 
-
-class BubbleSort
-{
-	 void bubbleSort(int arr[])
-    {
+class BubbleSort {
+    static void bubbleSort(int arr[]) {
         int n = arr.length;
         for (int i = 0; i < n - 1; i++)
             for (int j = 0; j < n - i - 1; j++)
                 if (arr[j] > arr[j + 1]) {
-                   
+
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
     }
-	
-	public static void main(String args[])
-	{
-		
-	bubbleSort();	
-	}
+
+    public static void main(String args[]) {
+
+        bubbleSort();
+    }
 }
